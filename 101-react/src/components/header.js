02d4b3ef427229ddb42045cc3e3b016803f83878
agungTuanany <agung.tuanany@gmail.com>
2018-05-28 00:24:03 +0700

    Syntectic Events

--- 101-react/src/components/header.js
+++ 101-react/src/components/header.js
@@ -2,11 +2,17 @@ import React, { Component } from "react";
 
 // TYPE OF COMPONENT: Class-Component
 class Header extends Component {
+    inputChange(event) {
+        console.log(event.target.value);
+    }
+
     render() {
         return (
             <header>
-                <div className="logo">logo</div>
-                <input />
+                <div onClick={() => console.log("clicked")} className="logo">
+                    logo
+                </div>
+                <input onChange={this.inputChange} />
             </header>
         );
     }
