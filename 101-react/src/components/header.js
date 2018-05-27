9db501dca2b3503a6b6dda6a6bd5a716dde878e5
agungTuanany <agung.tuanany@gmail.com>
2018-05-28 01:00:51 +0700

    Changing State

--- 101-react/src/components/header.js
+++ 101-react/src/components/header.js
@@ -1,3 +1,14 @@
+/**
+State is an object that servers us just like a databases so we can store values, check for events , in conclusion thrue the State we can control how the component behaves.
+
+@params .bind(this): this is refered to class, not to the function "this.setState()".
+    It's changing behavior of "this" function.
+    It's make a relationship with the function and actual class
+
+React is not allowed using :
+    this.state.push('')
+                ^^
+ */
 import React, { Component } from "react";
 
 // TYPE OF COMPONENT: Class-Component
@@ -11,7 +22,8 @@ class Header extends Component {
     }
 
     inputChange(event) {
-        console.log(event.target.value);
+        // console.log(event.target.value);
+        this.setState({ keywords: event.target.value });
     }
 
     render() {
@@ -20,7 +32,7 @@ class Header extends Component {
                 <div onClick={() => console.log("clicked")} className="logo">
                     logo
                 </div>
-                <input onChange={this.inputChange} />
+                <input onChange={this.inputChange.bind(this)} />
             </header>
         );
     }
