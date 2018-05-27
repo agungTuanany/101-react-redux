b3a2b8aca052bea3532f84a35290653e246b100a
agungTuanany <agung.tuanany@gmail.com>
2018-05-28 01:42:43 +0700

    working with Props

---
+++ 101-react/src/components/news_list.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const newsList = props => {
+    console.log(props);
+    return <div> News List </div>;
+};
+
+export default newsList;
--- 101-react/src/index.js
+++ 101-react/src/index.js
@@ -1,17 +1,28 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
 // COMPONENTS
 import Header from "./components/header";
+import JSON from "./db.json";
+import NewsList from "./components/news_list";
 
-// TYPE OF COMPONENT : Functional-Component
-const App = () => {
-    return (
-        <div>
-            <Header />
-            <h1>Hello world</h1>
-        </div>
-    );
-};
+// TYPE OF COMPONENT : Class-base-Component
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            news: JSON
+        };
+    }
+    render() {
+        return (
+            <div>
+                <Header />
+                <NewsList news={this.state.news} />
+            </div>
+        );
+    }
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
