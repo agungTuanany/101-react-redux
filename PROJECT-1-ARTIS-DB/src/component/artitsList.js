de0d861fa354ed3fe75d2a07cf2823438e914986
agungTuanany <agung.tuanany@gmail.com>
2018-05-28 15:19:25 +0700

    Fetching the list

---
+++ PROJECT-1-ARTIS-DB/src/component/artitsList.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const ArtistsList = props => {
+    console.log(props);
+    return <div> LIST ARTIS </div>;
+};
+
+export default ArtistsList;
--- PROJECT-1-ARTIS-DB/src/component/home.js
+++ PROJECT-1-ARTIS-DB/src/component/home.js
@@ -1,12 +1,37 @@
 import React, { Component } from "react";
 
 import Banner from "../component/banner";
+import ArtistsList from "./artitsList";
+
+const URL_ARTISTS = "http://localhost:3004/artists";
 
 class Home extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            artists: ""
+        };
+    }
+
+    componentDidMount() {
+        fetch(URL_ARTISTS, {
+            method: "GET"
+        })
+            .then(response => response.json())
+            .then(json => {
+                // console.log(json);
+                this.setState({
+                    artists: json
+                });
+            });
+    }
+
     render() {
         return (
             <div>
                 <Banner />
+                <ArtistsList allArtists={this.state.artists} />
             </div>
         );
     }
