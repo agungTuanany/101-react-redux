cf78ac0c1dfa5e29bd7c27e4cbbbd4637b48e7db
agungTuanany <agung.tuanany@gmail.com>
2018-05-28 19:39:37 +0700

    Styling the list

--- PROJECT-1-ARTIS-DB/src/component/artitsList.js
+++ PROJECT-1-ARTIS-DB/src/component/artitsList.js
@@ -2,33 +2,32 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const ArtistsList = props => {
-    // console.log(props);
-
     const list = ({ allArtists }) => {
         if (allArtists) {
             return allArtists.map(item => {
-                const style = {
+                const stlyes = {
                     background: `url('/images/covers/${
                         item.cover
-                    }.jpg) no-repeat`
+                    }.jpg') no-repeat`
                 };
 
                 return (
                     <Link
                         key={item.id}
-                        to={`/artists/${item.id}`}
+                        to={`/artist/${item.id}`}
                         className="artist_item"
-                        stle={style}
+                        style={stlyes}
                     >
-                        <div> {item.name} </div>
+                        <div>{item.name}</div>
                     </Link>
                 );
             });
         }
     };
+
     return (
         <div className="artists_list">
-            <h4> Browse the artists</h4>
+            <h4>Browse the artists</h4>
             {list(props)}
         </div>
     );
