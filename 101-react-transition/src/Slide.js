9e4e1c86e98433dd5716773c65bb1c9974f9f2ce
agungTuanany <agung.tuanany@gmail.com>
2018-05-29 20:25:50 +0700

    React Transition 01.Basic Set up

---
+++ 101-react-transition/src/Slide.js
@@ -0,0 +1,66 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class Slide extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            items: [99, 25]
+        };
+    }
+
+    addElements() {
+        return this.state.items.map(function(item, i) {
+            return (
+                <div className="item" key={i}>
+                    {item}
+                </div>
+            );
+        });
+    }
+
+    generateNumber() {
+        //let random = Math.floor(Math.random() * 100) + 1;
+        let newArray = [
+            ...this.state.items,
+            Math.floor(Math.random() * 100) + 1
+        ];
+
+        this.setState({
+            items: newArray
+        });
+    }
+
+    removeNumber() {
+        let newArray = this.state.items.slice(0, -1);
+        this.setState({
+            items: newArray
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                {this.addElements()}
+
+                <div className="btns">
+                    <div
+                        className="btn-add"
+                        onClick={() => this.generateNumber()}
+                    >
+                        Add Elements
+                    </div>
+                    <div
+                        className="btn-remove"
+                        onClick={() => this.removeNumber()}
+                    >
+                        Remove Elements
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Slide;
