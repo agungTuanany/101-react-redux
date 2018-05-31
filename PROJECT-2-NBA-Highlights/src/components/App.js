0fede73e9abde585729e0ef7a9747745913a9f93
agungTuanany <agung.tuanany@gmail.com>
2018-06-01 05:13:49 +0700

    Styling Header and Footer

--- PROJECT-2-NBA-Highlights/src/components/App.js
+++ PROJECT-2-NBA-Highlights/src/components/App.js
@@ -4,18 +4,19 @@ import { BrowserRouter, Route } from "react-router-dom";
 //COMPONENT
 import Header from "./header";
 import Footer from "./footer";
-import Home from "/home";
+import Home from "./home";
 
 class App extends Component {
     render() {
-        return;
-        <BrowserRouter>
-            <div>
-                <Header />
-                <Route exact path="/" component={Home} />
-                <Footer />
-            </div>
-        </BrowserRouter>;
+        return (
+            <BrowserRouter>
+                <div>
+                    <Header />
+                    <Route exact path="/" component={Home} />
+                    <Footer />
+                </div>
+            </BrowserRouter>
+        );
     }
 }
 
