22324047eaf41a48f68d29adb2a0e13a7a29b076
agungTuanany <agung.tuanany@gmail.com>
2018-06-02 23:55:56 +0700

    Create a Featured component for Slider and Style it

--- PROJECT-2-NBA-Highlights/src/components/home.js
+++ PROJECT-2-NBA-Highlights/src/components/home.js
@@ -1,8 +1,36 @@
 import React, { Component } from "react";
 
+// COMPONENT
+import Featured from "./featured";
+
+const URL_HOME = "http://localhost:3004/home";
+
 class Home extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            home: ""
+        };
+    }
+
+    componentDidMount() {
+        fetch(URL_HOME, { mehtod: "GET" })
+            .then(response => response.json())
+            .then(json => {
+                // console.log(json);
+                this.setState({
+                    home: json
+                });
+            });
+    }
+
     render() {
-        return <div> HOME </div>;
+        return (
+            <div>
+                <Featured slides={this.state.home.slider} />
+            </div>
+        );
     }
 }
 
