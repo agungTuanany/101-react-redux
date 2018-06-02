c824c1142f30b1f02e291792ed098c522c1e2965
agungTuanany <agung.tuanany@gmail.com>
2018-06-03 02:34:42 +0700

    Block Component 01.Showing the blocks

--- PROJECT-2-NBA-Highlights/src/components/blocks.js
+++ PROJECT-2-NBA-Highlights/src/components/blocks.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Reveal from "react-reveal/Reveal";
 
 const generateBlocks = ({ blocks }) => {
@@ -6,12 +7,19 @@ const generateBlocks = ({ blocks }) => {
         return blocks.map(item => {
             return (
                 <div key={item.id}>
-                    <Reveal effect="fadeInUp">
-                        <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing
-                            elit. Eos saepe, nemo consectetur vero dolorum
-                            officiis?
-                        </p>
+                    <Reveal effect="fadeInUp" className={`item ${item.type}`}>
+                        <div className="veil" />
+                        <div
+                            className="image"
+                            style={{
+                                background: `url(/images/blocks/${
+                                    item.image
+                                }) no-repeat`
+                            }}
+                        />
+                        <div className="title">
+                            <Link to={item.link}>{item.title}</Link>
+                        </div>
                     </Reveal>
                 </div>
             );
