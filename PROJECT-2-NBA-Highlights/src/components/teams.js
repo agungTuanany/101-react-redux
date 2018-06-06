6b2bd73e9fa522d79cb6d9b9f9f87c12612edde6
agungTuanany <agung.tuanany@gmail.com>
2018-06-06 20:30:18 +0700

    Creating Teams Component 03.Styling Teams

--- PROJECT-2-NBA-Highlights/src/components/teams.js
+++ PROJECT-2-NBA-Highlights/src/components/teams.js
@@ -29,64 +29,64 @@ class Teams extends Component {
             });
     }
 
-	searchTeam = event => {
-	    const keyword = event.target.value;
-	    if (keyword !== "") {
-	        const list = this.state.teams.filter(item => {
-	            return (
-	                item.name.toLowerCase().indexOf(keyword.toLowerCase()) > -1
-	            );
-	        });
-	        this.setState({
-	            filtered: list,
-	            keyword
-	        });
-	    } else {
-	        this.setState({
-	            filtered: this.state.teams,
-	            keyword
-	        });
-	    }
-	};
+    searchTeam(event) {
+        const keyword = event.target.value;
+        if (keyword !== "") {
+            const list = this.state.teams.filter(item => {
+                return (
+                    item.name.toLowerCase().indexOf(keyword.toLowerCase()) > -1
+                );
+            });
+            this.setState({
+                filtered: list,
+                keyword
+            });
+        } else {
+            this.setState({
+                filtered: this.state.teams,
+                keyword
+            });
+        }
+    }
 
-	renderList({ filtered }) {
-	    return filtered.map(item => {
-	        return (
-	            <CSSTransition key={item.id} {...fadeAnimations}>
-	                <Link
-	                    to={`/team/${item.name}`}
-	                    key={item.id}
-	                    className="team_item"
-	                >
-	                    <img
-	                        alt={item.name}
-	                        src={`/images/teams/${item.logo}`}
-	                    />
-	                </Link>
-	            </CSSTransition>
-	        );
-	    });
-	}
+    renderList({ filtered }) {
+        return filtered.map(item => {
+            return (
+                <CSSTransition key={item.id} {...fadeAnimations}>
+                    <Link
+                        to={`/team/${item.name}`}
+                        key={item.id}
+                        className="team_item"
+                    >
+                        <img
+                            alt={item.name}
+                            src={`/images/teams/${item.logo}`}
+                        />
+                    </Link>
+                </CSSTransition>
+            );
+        });
+    }
 
-	render() {
-	    return (
-	        <div className="teams_component">
-	            <div className="teams_input">
-	                <input
-	                    type="text"
-	                    value={this.state.keyword}
-	                    onChange={e => this.searchTeam(e)}
-	                    placeholder="Search for a Team"
-	                />
-	            </div>
-	            <div className="teams_container">
-	                <TransitionGroup>
-	                    {this.renderList(this.state)}
-	                </TransitionGroup>
-	            </div>
-	        </div>
-	    );
-	}
+    render() {
+        return (
+            <div className="teams_component">
+                <div className="teams_input">
+                    <input
+                        type="text"
+                        value={this.state.keyword}
+                        onChange={e => this.searchTeam(e)}
+                        placeholder="Search for a Team"
+                    />
+                </div>
+                <div className="teams_container">
+                    <TransitionGroup>
+                        {this.renderList(this.state)}
+                    </TransitionGroup>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Teams;
