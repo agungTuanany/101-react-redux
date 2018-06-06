5340e1cee5d8ba6613a5d4e6a94abe8dc7dc5779
agungTuanany <agung.tuanany@gmail.com>
2018-06-07 01:13:20 +0700

    Elementary setup React-Redux

---
+++ 101-redux-intermediate/src/index.js
@@ -0,0 +1,9 @@
+// MAIN index.js
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+// COMPONENT
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
