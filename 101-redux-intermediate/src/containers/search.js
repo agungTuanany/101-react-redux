9dc2eb0281304779c8a13902dc225bef4f2853cb
agungTuanany <agung.tuanany@gmail.com>
2018-06-07 14:40:09 +0700

    Creating Search

--- 101-redux-intermediate/src/containers/search.js
+++ 101-redux-intermediate/src/containers/search.js
@@ -1,11 +1,37 @@
 import React, { Component } from 'react';
 
 class Search extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            keyword: ''
+        };
+    }
+
+    searchCars = event => {
+        event.preventDefault();
+        console.log(this.state);
+    };
+    handleChange = event => {
+        this.setState({
+            keyword: event.target.value
+        });
+    };
+
+    componentDidAmount() {
+        console.log(this.state);
+    }
+
     render() {
         return (
             <div className="main_search">
-                <form>
-                    <input type="text" placeholder="input the car" />
+                <form onSubmit={this.searchCars}>
+                    <input
+                        type="text"
+                        placeholder="input the car"
+                        onChange={this.handleChange}
+                        value={this.state.keyword}
+                    />
                 </form>
             </div>
         );
