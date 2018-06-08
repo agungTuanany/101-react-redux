208abcc8bb2bb446af9b437632017073c3d8ba61
agungTuanany <agung.tuanany@gmail.com>
2018-06-08 21:44:52 +0700

    Home - Latest News 01.Create Latest Component

---
+++ PROJECT-3-News/src/components/home/Latest.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const LatestNews = props => {
+    return <div>FEATURED NEWS</div>;
+};
+
+export default LatestNews;
--- PROJECT-3-News/src/containers/Home.js
+++ PROJECT-3-News/src/containers/Home.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { latestNews } from '../actions';
+import { bindActionsCreators } from 'redux';
+
+// COMPONENTS
+import LatestNews from '../components/home/Latest';
 
 class Home extends Component {
     render() {
-        return <div>HOME</div>;
+        return (
+            <div>
+                <LatestNews />
+            </div>
+        );
     }
 }
+function mapStateToProps(state) {
+    return {
+        articles: state.articles
+    };
+}
 
-export default Home;
+function mapDispatchToProps(dispatch) {
+    return bindActionsCreators({ latestNewst }, dispatch);
+}
+export default connect(
+    mapStateToProps,
+    null
+)(Home);
