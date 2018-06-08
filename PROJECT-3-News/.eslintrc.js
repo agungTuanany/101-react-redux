3ef05004c22a2e5a210b376bcc14d48465f3de26
agungTuanany <agung.tuanany@gmail.com>
2018-06-08 19:02:20 +0700

    PROJECT-3-News Init

---
+++ PROJECT-3-News/.eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+	env: {
+		browser: true,
+		es6: true,
+		node: true
+	},
+	extends: [
+		"eslint:recommended",
+		"plugin:react/recommended",
+		"prettier",
+		"prettier/flowtype",
+		"prettier/react",
+		"prettier/standard"
+	],
+	plugins: ["react", "prettier", "flowtype"],
+	rules: {
+		indent: ["error", 4],
+		"no-tabs": 0,
+		"no-unused-vars": "off",
+		"class-methods-use-this": 0,
+		"import/no-named-as-default": 0,
+		"no-console": "off",
+		"react/jsx-filename-extension": [
+			"error",
+			{
+				extensions: [".js", ".jsx"]
+			}
+		]
+	},
+	parser: "babel-eslint",
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+			modules: true
+		}
+	}
+};
---
+++ PROJECT-3-News/src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+// TYPE OF COMPONENT: Class-Component
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {};
+    }
+
+    render() {
+        return <div> THIS IS A COMPONENT</div>;
+    }
+}
+
+export default App;
