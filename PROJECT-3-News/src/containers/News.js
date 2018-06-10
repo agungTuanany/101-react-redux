7b0a2741ffe3e64215890c6ae9bc8b4a55b550a2
agungTuanany <agung.tuanany@gmail.com>
2018-06-11 02:48:14 +0700

    News - Render Article (JSX)

--- PROJECT-3-News/src/containers/News.js
+++ PROJECT-3-News/src/containers/News.js
@@ -12,10 +12,46 @@ class News extends Component {
         this.props.clearSelectedNews();
     }
 
+    renderNews = ({ selected }) => {
+        if (selected) {
+            return selected.map(item => {
+                return (
+                    <div key={item.id}>
+                        <div className="tags">
+                            <span>
+                                <i className="fa fa-eye" />
+                                {item.views}
+                            </span>
+                            <span>
+                                <i className="fa fa-thumbs-up" />
+                                {item.likes[0]}
+                            </span>
+                            <span>
+                                <i className="fa fa-thumbs-down" />
+                                {item.likes[1]}
+                            </span>
+                        </div>
+                        <div>
+                            <h2>{item.title}</h2>
+                            <span>
+                                Article by:<strong>{item.author}</strong>
+                            </span>
+                        </div>
+                        <img
+                            alt={item.title}
+                            src={`/images/articles/${item.img}`}
+                        />
+                        <div>{item.body}</div>
+                    </div>
+                );
+            });
+        }
+    };
+
     render() {
         return (
-            <div>
-                <h3>THIS IS NEWS ARTICLES</h3>
+            <div className="news-container">
+                {this.renderNews(this.props.articles)}
             </div>
         );
     }
