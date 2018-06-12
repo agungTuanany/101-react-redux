5cba4306a1b0a207818ceedc394d73be56afcadd
agungTuanany <agung.tuanany@gmail.com>
2018-06-12 17:47:54 +0700

    Redux - Form 03.Validating in Redux

--- 101-redux-form/src/Form.js
+++ 101-redux-form/src/Form.js
@@ -8,6 +8,7 @@ class Form extends Component {
             <div className="form-input">
                 <label>{field.myLabel}</label>
                 <input type="text" {...field.input} />
+                <div className="error">{field.meta.error}</div>
             </div>
         );
     }
@@ -17,6 +18,7 @@ class Form extends Component {
             <div className="from-input">
                 <label>{field.myLabel}</label>
                 <textarea {...field.input} />
+                <div className="error">{field.meta.error}</div>
             </div>
         );
     }
@@ -40,7 +42,7 @@ class Form extends Component {
 
                     <Field
                         myLabel="From"
-                        name="form"
+                        name="from"
                         component={this.renderInputFiled}
                     />
 
@@ -55,6 +57,25 @@ class Form extends Component {
     }
 }
 
+function validate(values) {
+    const errors = {};
+
+    if (!values.title) {
+        errors.title = 'The title is empty';
+    }
+
+    if (!values.from) {
+        errors.from = 'Enter your name';
+    }
+
+    if (!values.message) {
+        errors.message = 'Enter a Message';
+    }
+
+    return errors;
+}
+
 export default reduxForm({
+    validate,
     form: 'PostMessage'
 })(Form);
